aa9b2039fda55f05e5caf933501feb5403ae792d
TIPYexe <tipy_serios@yahoo.com>
2021-03-05 14:53:28 +0200

    Add Timestamp

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from datetime import date
 from datetime import datetime
+from datetime import timedelta
 import os
 
+# inlocuieste aici cu path-ul la care sa se salveze fisierele
 highlight_path = 'G:/Youtuber mare/Streaming/Highlight-timestamp/'
-today = date.today()
 
+# data de azi
+today = date.today()
+# sablon pt ora:minut:secunda
+FMT = '%H:%M:%S'
+
+# cand sciptul este apelat pt prima oara, el va creea un fisier cu numele = data la care facem stream-ul
+# in care adaugam ora la care am inceput streamul
 if not os.path.exists(highlight_path + str(today) + '.txt'):
     file = open(highlight_path + str(today) + '.txt', 'a+')
     now = datetime.now()
-    file.write(str(today) + '\n')
-    file.write(str(now.hour) + ':' + str(now.minute) + ':' + str(now.hour) + '\n')
+    file.write(str(now.hour) + ':' + str(now.minute) + ':' + str(now.second) + '\n')
+    file.write('\nMomente:\n')
     file.close()
+
+# daca fisierul deja exista (deci suntem live) vrem sa adaugam minutul si secunda din stream la care se afla
+# highlight-ul
+else:
+    file = open(highlight_path + str(today) + '.txt', 'r')
+    now = datetime.now()
+    string_now = str(now.hour) + ':' + str(now.minute) + ':' + str(now.second)
+    contents = file.readlines()
+    contents[0] = contents[0].replace('\n', '')
+    timestamp = datetime.strptime(string_now, FMT) - datetime.strptime(contents[0], FMT)
+
+    # atunci cand facem stream peste 12 noaptea, ora actuala ar fi mai mica decat cea de inceput si ne-ar da
+    # calculele peste cap
+    # if timestamp.days < 0:
+    #     timestamp = timedelta(days=0, seconds=timestamp.seconds)
+
+    file.close()
+    file_a = open(highlight_path + str(today) + '.txt', 'a+')
+    file_a.write(str(timestamp) + '\n')
+    file_a.close()
+
+# TODO:
+#   - daca intrerup un stream si il reincep in aceeasi zi, sa am fisiere separate cu index la final
+#   ex: 10-02-2021-0 | 10-02-2021-1
