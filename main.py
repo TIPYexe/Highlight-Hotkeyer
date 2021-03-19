d15c0f0feaf25ea50a71a448af5a85033b386b5e
TIPYexe <tipy_serios@yahoo.com>
2021-03-19 02:56:25 +0200

    Past Mid night error fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 import os
 
 # inlocuieste aici cu path-ul la care sa se salveze fisierele
-highlight_path = 'G:/Youtuber mare/Streaming/Highlight-timestamp/'
+highlight_path = 'G:/Youtuber-mare/Streaming/Highlight-timestamp/'
 
 # data de azi
 today = datetime.today()
@@ -12,14 +12,19 @@ today_formated = datetime.strftime(today, '%d-%m-%Y')
 # sablon pt ora:minut:secunda
 FMT = '%H:%M:%S'
 
-# cand sciptul este apelat pt prima oara, el va creea un fisier cu numele = data la care facem stream-ul
-# in care adaugam ora la care am inceput streamul
-if not os.path.exists(highlight_path + today_formated + '.txt'):
-    file = open(highlight_path + today_formated + '.txt', 'a+')
-    now = datetime.now()
-    file.write(str(now.hour) + ':' + str(now.minute) + ':' + str(now.second) + '\n')
-    file.write('\nMomente:\n')
-    file.close()
+curr_hour = datetime.now().hour
+
+# verific daca e trecut de ora 10 cand rulez scriptul, pentru ca altfel
+# si daca il rulez la 1 noaptea, mi-ar fi creat un fisier nou, si nu asta vreau
+if curr_hour > 10:
+    # cand sciptul este apelat pt prima oara, el va creea un fisier cu numele = data la care facem stream-ul
+    # in care adaugam ora la care am inceput streamul
+    if not os.path.exists(highlight_path + today_formated + '.txt'):
+        file = open(highlight_path + today_formated + '.txt', 'a+')
+        now = datetime.now()
+        file.write(str(now.hour) + ':' + str(now.minute) + ':' + str(now.second) + '\n')
+        file.write('\nMomente:\n')
+        file.close()
 
 # daca fisierul deja exista (deci suntem live) vrem sa adaugam minutul si secunda din stream la care se afla
 # highlight-ul
